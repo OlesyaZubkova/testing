eb1e1695e47fd08b3348c7b1e3d1c3cb4c518063
OlesyaZubkova <69164989+OlesyaZubkova@users.noreply.github.com>
2023-08-22 12:50:55 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false;
+  } else {
+    let partPercent = percent / 1200,
+        creditBody = amount - contribution, // тело кредита — сумма кредита минус первоначальный взнос
+        payment = creditBody * (partPercent + (partPercent / ((Math.pow(1 + partPercent, countMonths)) - 1))), // ежемесячная оплата
+        sum = +(payment * countMonths).toFixed(2); // общая сумма, которую придётся заплатить клиенту
+    return sum;
+  }
 }
