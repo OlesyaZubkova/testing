3a1eb796ff9eb4e84c04878e5684ee293a0a363b
OlesyaZubkova <69164989+OlesyaZubkova@users.noreply.github.com>
2023-09-20 15:55:50 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,24 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    let cache = [];
+    return function wrapper(...args) {
+      const hash = md5(args);
   
-}
+      let objectInCache = cache.find((item) => item.hash === hash);
+      if (objectInCache) {
+        console.log("Из кэша: " + objectInCache.result);
+        return "Из кэша: " + objectInCache.result;
+      } else {
+        let result = func.call(this, ...args);
+        cache.push({hash, result});
+        if (cache.length > 5) {
+          cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result
+      }
+    }
+  }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
